42170aec1a6be05f9dcbe9b8bc105c3976bdb371
Victor Santos <menezes.victorsantos@outlook.com>
2026-01-19 02:32:59 -0300

    fix: submit button disabled state and form error when the video uploaded is removed

--- components/video/options.tsx
+++ components/video/options.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useFormContext } from "react-hook-form";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { RainbowButton } from "../ui/rainbow-button";
@@ -10,30 +12,42 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { useVideoStore } from "./preview/video-store";
+import { Field, FieldDescription, FieldLabel, FieldLegend } from "../ui/field";
 
 export function Options() {
+  const { isReady } = useVideoStore();
   return (
     <div className="flex flex-col gap-4 h-full justify-between">
       <div className="flex flex-col gap-3 ">
-        <Label>Resolution</Label>
-        <Select>
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Select a resolution" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Resolutions available</SelectLabel>
-              <SelectItem value="1080">1080p</SelectItem>
-              <SelectItem value="720">720p</SelectItem>
-              <SelectItem value="480">480p</SelectItem>
-              <SelectItem value="360">360p</SelectItem>
-              <SelectItem value="240">240p</SelectItem>
-              <SelectItem value="144">144p</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <Field>
+          <FieldLabel>Presets</FieldLabel>
+          <FieldDescription>
+            Higher values means more compression.
+          </FieldDescription>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select the compress level preset" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Presets</SelectLabel>
+                <SelectItem value="very_high">Very High</SelectItem>
+                <SelectItem value="high">High</SelectItem>
+                <SelectItem value="medium">Medium</SelectItem>
+                <SelectItem value="low">low</SelectItem>
+                <SelectItem value="very_low">Very low</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </Field>
       </div>
-      <Button variant={"default"} type="submit" form="video-form">
+      <Button
+        variant={"default"}
+        type="submit"
+        form="video-form"
+        disabled={!isReady}
+      >
         Compress file
       </Button>
     </div>
