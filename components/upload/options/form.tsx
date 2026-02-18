0ac906f3b61281a1dc5b7b0840f65a72703921b9
Victor Santos <menezes.victorsantos@outlook.com>
2026-02-18 06:30:43 -0300

    fix: resolvers path bug

--- components/upload/options/form.tsx
+++ components/upload/options/form.tsx
@@ -1,5 +1,5 @@
 import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"
 import {
   Field,
   FieldDescription,
